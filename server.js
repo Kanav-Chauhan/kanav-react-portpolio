c656642c9dd467de02c08eb6102e40d2a8f64d66
Kanav Chauhan <kanavchauhan001@gmail.com>
2023-04-28 16:23:33 +0530

    mail pass

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: "kanavchauhan001@gmail.com",
-    pass: "vkwjojlpatxzuevr"
+    pass: "nckcubawcueyfrik"
   },
 });
 
